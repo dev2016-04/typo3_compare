8cc58be9d731867606778711035512b0ed24b039
dev2016-4 <dev2016_4flow@openmailbox.org>
2016-05-03 17:19:22 +0200

    jfmulticontent changes

--- jfmulticontent/ext_emconf.php
+++ jfmulticontent/ext_emconf.php
@@ -1,16 +1,16 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "jfmulticontent".
-#
-# Auto generated 15-08-2015 17:56
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "jfmulticontent".
+ *
+ * Auto generated 12-04-2013 13:50
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Multiple Content',
 	'description' => 'Arranges multiple contents into one content element with multiple columns, accordions, tabs, slider, slidedeck, easyAccordion or Booklet (Sponsored by http://www.made-in-nature.de/typo3-agentur.html). This extension will also extend tt_news with two new lists. Use t3jquery for better integration with other jQuery extensions.',
 	'category' => 'plugin',
@@ -20,8 +20,6 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '4.3.0-6.0.4',
-	'PHP_version' => '5.0.0-0.0.0',
 	'module' => '',
 	'state' => 'stable',
 	'uploadfolder' => 0,
@@ -32,8 +30,21 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Juergen Furrer',
 	'author_email' => 'juergen.furrer@gmail.com',
 	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'cms' => '',
+			'php' => '5.0.0-0.0.0',
+			'typo3' => '4.3.0-6.0.4',
+		),
+		'conflicts' => '',
+		'suggests' => 
+		array (
+		),
+	),
 );
 
 ?>
