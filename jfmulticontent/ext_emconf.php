587b226f072321afac81606854953f0c5ebcd634
dev2016-4 <dev2016_4flow@openmailbox.org>
2016-05-03 17:17:34 +0200

    jfmulticontent

---
+++ jfmulticontent/ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "jfmulticontent".
+#
+# Auto generated 15-08-2015 17:56
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Multiple Content',
+	'description' => 'Arranges multiple contents into one content element with multiple columns, accordions, tabs, slider, slidedeck, easyAccordion or Booklet (Sponsored by http://www.made-in-nature.de/typo3-agentur.html). This extension will also extend tt_news with two new lists. Use t3jquery for better integration with other jQuery extensions.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '2.9.3',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '4.3.0-6.0.4',
+	'PHP_version' => '5.0.0-0.0.0',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Juergen Furrer',
+	'author_email' => 'juergen.furrer@gmail.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
